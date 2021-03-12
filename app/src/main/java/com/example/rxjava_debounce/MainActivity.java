4ffa3f2ce50d8b3fc890860243d9e9d7cbdd031b
armanziaei <armanziaei83@gmaio.com>
2021-03-12 13:11:49 +0330

    Feat ViewModel : MainViewModel class created, initialized in mainActivity

--- app/src/main/java/com/example/rxjava_debounce/MainActivity.java
+++ app/src/main/java/com/example/rxjava_debounce/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.rxjava_debounce;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.widget.SearchView;
@@ -8,7 +9,7 @@ import android.widget.SearchView;
 public class MainActivity extends AppCompatActivity {
 
     SearchView searchView;
-
+    MainViewModel mainViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
         searchView = findViewById(R.id.searchBox);
 
-        
+        mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+
     }
 }
---
+++ app/src/main/java/com/example/rxjava_debounce/MainViewModel.java
@@ -0,0 +1,11 @@
+package com.example.rxjava_debounce;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+
+    public void prepareObservables (){
+
+
+    }
+}
